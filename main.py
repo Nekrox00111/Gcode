0efb2d2c4f8c1fb28d060af5af9e7013c0447c7d
Gonzalo Valle <grvi1999@gmail.com>
2022-01-31 15:27:00 -0600

    Avance2 de proyecto de Graduacion

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from settings import MACHINE_SETTINGS
-#from prueba import CNC
-
-tool_position = []
-
 class CNC:
+    
     code = ''
-
-    def change_tool(self,tool_position):
-        self.code += f'T{tool_position[1]} M6 \n' 
-        #Recordar que 'H' y 'D' son la compensacion de Altura y Diametro con el mismo "tool_position"
-
-    def __init__(self,x,y,position,units,tools,id_proyecto,project_name):
-
+    z=0
+    tools=[]
+    def __init__(self,x,y,position,units,tools,id_proyecto,project_name,depth,z):
+        self.z = z
+        self.tools = tools
+        self.selected_tool=self.tools[0]
         units1 = 90
         position1 = 20
-        # if units == "pulgadas":
-        #     units1 = 90 # pulgadas
-        # else:
-        #     units1 = 91 # milimetros
-
-        # if position == "absoluto":
-        #     position1 = 20 # absoluto
-        # else:
-        #     position1 = 21 #incremental
 
         if units != "pulgadas":
             units1 = 91 # milimetros
@@ -30,20 +16,88 @@ class CNC:
             position1 = 21 # incremental  
         
         self.code += f'O{id_proyecto} ({project_name}) \n'
-        self.code += f'T{tools[0]["id"]} M06 \n' 
-        #self.code += f'T{MACHINE_SETTINGS["tools"][0]["id"]} M06 \n' 
+        self.code += f'T{self.selected_tool["id"]} M06 ({self.selected_tool["name"]})\n' 
         self.code += f'G{units1} G{position1} G54 X{x} Y{y}\n'
-        self.code += f'S{tools[0]["rpm"]} M03\n'
-
+        self.code += f'S{self.selected_tool["rpm"]} M03\n'
+        self.code += f'G43 H{self.selected_tool["h"]} Z{z}\n'
+        self.code += f'G01 Z{depth} F{self.selected_tool["fv"]}\n'
 
     def print_code(self):
         lines = self.code.splitlines()
         code = ''
+        print()
         for idx, line in enumerate(lines):
-            code += f'N{idx+1}    {line}\n'
+            code += f'N{idx}    {line}\n'
 
         print(code)
 
+
+    def cut_line(self,x,y):
+        self.code += f'G01 X{x} Y{y}\n'
+    
+    def cut_vertical(self,depth):
+        self.code += f'G01 Z{depth} F{self.selected_tool["fv"]}\n'
+    
+    def arc_cut(self,x,y,clockwise,r):
+        
+        if clockwise == True:
+            self.code += f'G02 X{x} Y{y} R{r}\n'
+        else:
+            self.code += f'G03 X{x} Y{y} R{r}\n'
+
+
+    def change_tool(self,tools1,x,y):
+        
+        tool_found = [tool for tool in self.tools if tool.get('id') == tools1]
+        if not tool_found:
+            tool_found = [tool for tool in self.tools if tool.get('name') == tools1]
+        if not tool_found:
+            tool_found = [tool for tool in self.tools if tool.get('label') == tools1]
+        if not tool_found:
+            raise Exception('¡No se encontro herramienta deseada!')
+            
+        self.selected_tool = tool_found[0]
+
+        self.code += f'G00 Z{self.z} \n' 
+        self.code += f'G53 G49 Z0 M05 \n'
+        self.code += f'T{self.selected_tool["id"]} M06 ({self.selected_tool["name"]})\n' 
+        self.code += f'G54 G00 X{x} Y{y} \n'
+        self.code += f'S{self.selected_tool["rpm"]} M03 \n'
+        self.code += f'G43 H{self.selected_tool["h"]} Z{self.z} \n' 
+    
+    def empty_circle(self,clockwise,x,y,depth,r):
+        if clockwise == True:
+            self.code += f'G12 X{x} Y{y} Z{depth} I{r} D{self.selected_tool["d"]} F{self.selected_tool["fh"]}\n'
+        else:
+            self.code += f'G13 X{x} Y{y} Z{depth} I{r} D{self.selected_tool["d"]} F{self.selected_tool["fh"]}\n'
+        self.code += f'G00 Z{self.z} G40 \n'
+
+    # def empty_spiral(self,clockwise,x,y,depth,r):
+    #     if clockwise == True:
+    #         self.code += f'G12 X{x} Y{y} Z{depth} I{r} D{self.selected_tool["d"]} F{self.selected_tool["fh"]}\n'
+    #     else:
+    #         self.code += f'G13 X{x} Y{y} Z{depth} I{r} D{self.selected_tool["d"]} F{self.selected_tool["fh"]}\n'
+    #     self.code += f'G00 Z{z} G40 \n'
+
+    def move(self,x,y):
+        self.code += f'G00 X{x} Y{y} G40 \n'
+    
+    def compensacion_ala(self,derecha,x,y):
+        if derecha == True:
+            self.code += f'G42 X{x} Y{y} D{self.selected_tool["d"]} F{self.selected_tool["fh"]} \n'
+        else:
+            self.code += f'G41 X{x} Y{y} D{self.selected_tool["d"]} F{self.selected_tool["fh"]} \n'
+
+
+    def exit(self,position):
+        position1 = 91
+        
+        if position != "incremental":
+            position1 = 90 # absoluto
+        
+        self.code += f'G00 Z{self.z} G40\n'
+        self.code += f'G28 G{position1} Z0.0 M05\n'
+        self.code += f'M30'
 
 
 if __name__ == '__main__':
--- prueba.py
+++ prueba.py
@@ -1,49 +1,93 @@
-from main    import CNC
+from main import CNC
 
 T1 = {
-    'label': 'Excavadora corta',
-    'name': 'Excavator23',
+    'name': 'Fresa Plana de 0.3"',
+    'label': 'T1',
     'rpm': '7000',
-    'h': 23,
-    'id': 23,
+    'h': '03',
+    'd': '03',
+    'id': '03',
+    'fv': 1.4,
+    'fh': 4.0
 }
 T2 = {
-    'label': 'Excavadora larga',
-    'name': 'Excavator44',
-    'h': 44,
-    'id': 44,
+    'name': 'Fresa Plana de 0.25"',
+    'label': 'T2',
+    'rpm': '6500',
+    'h': '01',
+    'd': '01',
+    'id': '01',
+    'fv': 1.6,
+    'fh': 5.0
 }
 
 machine = CNC(
-    id_proyecto=1234,
+    id_proyecto=5555,
     project_name="Aleta de Enfriamiento",
-    x=1,
-    y=2,
-    position='incremental',
-    units='milimetro',
-    tools=[T1,T2]
+    x=2.5,
+    y=3.1,
+    z=0.25,
+    position='absoluto',
+    units='pulgadas',
+    tools=[T1,T2],
+    depth = -0.15
 )
 
+machine.cut_line(
+    x=1.5,
+    y=3.2
+)
 
-# fh ya la sabe la maquina
-# machine.empty_circle(
+machine.arc_cut(
+    clockwise=False,
+    x=3.5,
+    y=5.2,
+    r=2.0
+)
+
+machine.change_tool(
+    x=0.5,
+    y=1.5,
+    tools1 = 'T2'
+)
+
+machine.empty_circle(
+    x=1,
+    y=2,
+    depth=-0.15,
+    r=3/8,
+    clockwise=False
+)
+# levanta la herramienta automaticamente
+
+# machine.empty_spiral(
 #     x=1,
 #     y=2,
-#     depth=0.15,
+#     depth=-0.15,
 #     r=3/8,
-#     clockwise=False
+#     clockwise=False,
 # )
-# levantar pieza ya lo hace automaticamente
-# machine.empty_circle(
-#     x=3,
-#     y=2,
-#     depth=0.15,
-#     r=5/8,
-# )
-# machine.change_tool(T2)
-# machine.move(x=3, y=0)
-# machine.cut_line((-0.4,0.2), (0.4,0.6))
-# machine.stop()
+
+
+machine.move(
+    x=3,
+    y=0
+)
+
+machine.cut_vertical(
+    depth = -0.15 
+)
+
+machine.compensacion_ala(
+    derecha = True,
+    x=-1,
+    y=-1
+)
+
+machine.exit(
+    position ="incremental"
+)
 
 machine.print_code()
+
 # machine.export('prueba.gcode')
